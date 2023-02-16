fbf1b2f8ee101bd797746ad1911fa8bd620099a1
eduhdev12 <eduhdev12@users.noreply.github.com>
2023-02-16 09:55:15 +0200

    feat(channels): added test route

--- src/channels/channels.controller.ts
+++ src/channels/channels.controller.ts
@@ -1,16 +1,22 @@
-import { ConsoleLogger, Controller, Get, UseGuards, Request } from "@nestjs/common";
-import { Role, Roles } from "src/auth/decorators/roles.decorators";
+import {
+  ConsoleLogger,
+  Controller,
+  Get,
+  Request,
+  UseGuards,
+} from "@nestjs/common";
 import { AuthentificatedGuard } from "src/auth/guards/authentificated.guard";
 import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
+import { ChannelsService } from "./channels.service";
 
 @Controller("channels")
 export class ChannelsController {
   private readonly logger = new ConsoleLogger(ChannelsController.name);
+  constructor(private readonly channelsService: ChannelsService) {}
 
   @UseGuards(AuthentificatedGuard, JwtAuthGuard)
-//   @Roles(Role.PREMIUM)
   @Get("/testadmin")
   async testAdmin(@Request() req) {
-    return { message: "You are premium user" };
+    return this.channelsService.getChannels(req.user.id);
   }
 }
--- src/channels/channels.service.ts
+++ src/channels/channels.service.ts
@@ -6,5 +6,11 @@ export class ChannelsService {
   private readonly logger = new ConsoleLogger(ChannelsService.name);
   constructor(private prisma: PrismaService) {}
 
-  
+  async getChannels(userId: number) {
+    let channelData = await this.prisma.channel.findMany({
+      where: { users: { some: { id: userId } } },
+    });
+
+    return channelData;
+  }
 }
