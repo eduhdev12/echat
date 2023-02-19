2d3589cda2ff1b4ebf009ef8730a9632b0923a59
eduhdev12 <eduhdev12@users.noreply.github.com>
2023-02-19 19:38:06 +0200

    feat(sockets): more dev

--- src/channels/channels.gateway.ts
+++ src/channels/channels.gateway.ts
@@ -1,11 +1,12 @@
 import {
   ConnectedSocket,
+  MessageBody,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from "@nestjs/websockets";
-import { Socket } from "socket.io";
-import { Server } from "ws";
+import { Socket, Server } from "socket.io";
+import { PrismaService } from "src/prisma.service";
 
 @WebSocketGateway({
   cors: { origin: "http://localhost:5173" },
@@ -14,8 +15,18 @@ export class ChannelsGateway {
   @WebSocketServer()
   server: Server;
 
-  @SubscribeMessage("message")
-  handleMessage(@ConnectedSocket() client: Socket, payload: any): string {
+  constructor(private prisma: PrismaService) {}
+
+  @SubscribeMessage("joinRoom")
+  async handleMessage(@ConnectedSocket() client: Socket, @MessageBody() channelId: string): Promise<string> {
+    console.log("client joined room")
+    client.join("1")
+    
     return "Hello world!";
+  }
+
+  @SubscribeMessage("trigger")
+  triggerMessage(@ConnectedSocket() client: Socket, payload: any) {
+    this.server.to("1").emit("newMessage", "testmsg123");
   }
 }
